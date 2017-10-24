5ae3e94a8fe06222b2b7d25b0a33af285549e458
Ilya Lipai <Ilya_Lipai@epam.com>
2017-10-24 11:46:59 +0300

    ConsoleTest For NumConverter

---
+++ NumConverter.ConsoleTests/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumConverter.ConsoleTests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Write 'Exit' to exit programm");
+
+            string input = string.Empty;
+            int resultNumber = 0;
+
+            do
+            {
+                input = Console.ReadLine();
+
+                input = input?.Trim(' ');
+
+                if (NumHelper.TryParse(input,out resultNumber))
+                {
+                    Console.WriteLine($"Number from string: {resultNumber}");
+                }
+                else
+                {
+                    Console.WriteLine($"Can not cinvert string: {input} to Int32");
+                }
+
+            } while (!input.ToLower().Equals("exit"));
+        }
+    }
+}
